c0f4daa0fb505e3302ce7dd6c7d2f64f6b9e1107
Hao Fu <jamesfuhao@gmail.com>
2015-10-16 15:52:30 -0700

    ReverseLinkedListII

--- src/LinkedList/ReverseLinkedListII.java
+++ src/LinkedList/ReverseLinkedListII.java
@@ -6,39 +6,24 @@ public class ReverseLinkedListII {
      * @oaram m and n
      * @return: The head of the reversed ListNode
      */
-    public ListNode reverseBetween(ListNode head, int m , int n) {
-    	if (m > n) {
-    		return null;
-    	}
-    	
-    	if (m == 1) {
-    		return reverse(head, n);
-    	}
-    	
-    	ListNode dummy = head;
-    	for (int i = 1; head != null && i <= m - 1; i++) {
-    		if (i == m - 1) {
-    			head.next = reverse(head.next, n - m + 1);
-    		} else {
-    			head = head.next;
-    		}
-    	}
-    	
-    	return dummy;
-    }
-    
-	private ListNode reverse(ListNode head, int n) {
-		ListNode prev = head, tail = head;
-		for (int i = 0; i < n; i++) {
+	public ListNode reverseBetween(ListNode head, int m, int n) {
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		head = dummy;
+		for (int i = 0; i < m - 1; i++) {
+			head = head.next;
+		}
+		ListNode start = head, prev = null;
+		head = head.next; ListNode rear = head;
+		for (int i = m; i <= n; i++) {
 			ListNode tmp = head.next;
 			head.next = prev;
 			prev = head;
-			if (i != n - 1) {
-				head = tmp;
-			} else {
-				tail.next = tmp;
-			}
+			head = tmp;
 		}
-		return head;
+		start.next = prev;
+		rear.next = head;
+
+		return dummy.next;
 	}
 }
