c3f0dc5155ecebc4c6b91d554f8a55a4a46726df
HaoFu <jamesfuhao@gamil.com>
2015-10-20 12:44:33 -0700

    longest rectangle in histogram

--- src/DataStructure/Stack/LargestRectangleinHistogram.java
+++ src/DataStructure/Stack/LargestRectangleinHistogram.java
@@ -14,19 +14,20 @@ public class LargestRectangleinHistogram {
 			return 0;
 		}
 		Stack<Integer> stack = new Stack<Integer>();
-		int[] tmp = new int[height.length + 1];
-		for (int i = 0; i < height.length; i++) {
-			tmp[i] = height[i];
+		int[] tmp = new int[height.length + 2];
+		tmp[0] = -2;
+		for (int i = 1; i <= height.length; i++) {
+			tmp[i] = height[i - 1];
 		}
-		tmp[height.length] = -1;
+		tmp[tmp.length - 1] = -1;
 		height = tmp;
 
 		int res = 0;
 		stack.push(0);
 		for (int i = 1; i < height.length; i++) {
-			while (!stack.isEmpty() && height[i] < height[stack.peek()]){
-				int index = stack.pop();
-				int size = (i - index) * height[index];
+			while (height[i] < height[stack.peek()]){
+				int tmpHeight = height[stack.pop()];
+				int size = (i - stack.peek() - 1) * tmpHeight;
 				res = Math.max(res, size);
 			}
 			stack.push(i);
