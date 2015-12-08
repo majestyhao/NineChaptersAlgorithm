59558aba19e71c2a260c4fbaba1839a292a1232e
HaoFu <jamesfuhao@gamil.com>
2015-12-07 19:47:18 -0800

    find pos

---
+++ src/BinarySearch/FirstPositionofTarget.java
@@ -0,0 +1,34 @@
+package BinarySearch;
+
+/**
+ * Created by hao on 15-12-7.
+ */
+public class FirstPositionofTarget {
+    /**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearch(int[] nums, int target) {
+        int start = 0, end = nums.length;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                end = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (nums[start] == target) {
+            return start;
+        }
+        if (nums[end] == target) {
+            return end;
+        }
+
+        return -1;
+    }
+}
