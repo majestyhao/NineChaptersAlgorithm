7bcec6ecaa37ebe99757e3ea10ac910edaf0c6a2
HaoFu <jamesfuhao@gamil.com>
2015-12-09 15:16:01 -0800

    edit distance

--- src/DynamicProgrammingI/JumpGame.java
+++ src/DynamicProgrammingI/JumpGame.java
@@ -2,30 +2,27 @@ package DynamicProgrammingI;
 
 public class JumpGame {
 	/**
-     * @param A: A list of integers
-     * @return: The boolean answer
-     */
-	public boolean canJump(int[] A) {
-    	int n = A.length;
-    	// do not use int[], since f[i] == 0 + A[j - 1] still
-    	// possible to >= i
-    	boolean[] f = new boolean[n + 1]; 
-    	
-    	// init
-    	f[0] = true;
-    	f[1] = true;
-    	
-    	// loop
-    	for (int i = 2; i <= n; i++) {
-    		f[i] = false;
-    		for (int j = 1; j < i; j++) {
-    			if (f[j] == true && (j + A[j - 1] >= i)) {
-    				f[i] = true;
-    			}
-    		}
-    	}
-    	
-    	return f[n];
-    }
+	 * @param nums: A list of integers
+	 * @return: The boolean answer
+	 */
+	public boolean canJump(int[] nums) {
+		int n = nums.length;
+
+		// init
+		boolean[] f = new boolean[n + 1];
+		f[0] = true;
+		f[1] = true;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j < i; j++) {
+				if (f[j] && j + nums[j - 1] >= i) {
+					f[i] = true;
+					break;
+				}
+			}
+		}
+
+		return f[n];
+	}
 
 }
--- src/DynamicProgrammingII/EditDistance.java
+++ src/DynamicProgrammingII/EditDistance.java
@@ -2,61 +2,34 @@ package DynamicProgrammingII;
 
 public class EditDistance {
 	/**
-     * @param word1 & word2: Two string.
-     * @return: The minimum number of steps.
-     */
-    public int minDistance(String word1, String word2) {
-    	int m = word1.length();
-    	int n = word2.length();
-    	
-    	if (m == 0 && n == 0) {
-    		return 0;
-    	} else if (m == 0) {
-    		return n;
-    	} else if (n == 0) {
-    		return m;
-    	}
-    	
-    	// f: 从A[0~i]变成B[0~j]需要的最小步数
-    	int[][] f = new int[m][n];
-    	
-    	// **init
-    	boolean flag = true;
-    	for (int i = 0; i < m; i++) {
-    		if (word1.charAt(i) == word2.charAt(0)) {
-    			flag = false;
-    		}
-    		if (flag) {
-    			f[i][0] = i + 1;
-    		} else {
-    			f[i][0] = i;
-    		}
-    	}
-    	flag = true;
-    	for (int j = 1; j < n; j++) {
-    		if (word1.charAt(0) == word2.charAt(j)) {
-    			flag = false;
-    		}
-    		if (flag) {
-    			f[0][j] = j + 1;
-    		} else {
-    			f[0][j] = j;
-    		}
-    	}
-    	
-    	// loop
-    	for (int i = 1; i < m; i++) {
-    		for (int j = 1; j < n; j++) {
-    			int edit = f[i - 1][j - 1];
-    			if (word1.charAt(i) != word2.charAt(j)) {
-    				edit += 1;
-    			}
-    			// 分别对应着A[0~i]通过del(A[i]), insert(B[j])或edit(A[i] => B[j])变成B[0~j]的步数
-    			f[i][j] = Math.min(Math.min(f[i - 1][j], f[i][j - 1]) + 1, edit);
-    		}
-    	}
-    	
-    	// end
-    	return f[m - 1][n - 1];
-    }
+	 * @param word1 & word2: Two string.
+	 * @return: The minimum number of steps.
+	 */
+	public int minDistance(String word1, String word2) {
+		int m = word1.length();
+		int n = word2.length();
+
+		// init
+		int[][] f = new int[m + 1][n + 1];
+		f[0][0] = 0;
+		for (int i = 1; i <= m; i++) {
+			f[i][0] = i;
+		}
+		for (int j = 1; j <= n; j++) {
+			f[0][j] = j;
+		}
+
+		// loop
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				int edit = f[i - 1][j - 1];
+				if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+					edit += 1;
+				}
+				f[i][j] = Math.min(edit, Math.min(f[i - 1][j] + 1, f[i][j - 1] + 1));
+			}
+		}
+
+		return f[m][n];
+	}
 }
