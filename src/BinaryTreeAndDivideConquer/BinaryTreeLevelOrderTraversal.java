2187a5851ce79810df39701bb6c0650a578bf9c8
HaoFu <jamesfuhao@gamil.com>
2015-12-08 18:58:17 -0800

    level order

--- src/BinaryTreeAndDivideConquer/BinaryTreeLevelOrderTraversal.java
+++ src/BinaryTreeAndDivideConquer/BinaryTreeLevelOrderTraversal.java
@@ -6,43 +6,32 @@ import java.util.ArrayList;
 
 public class BinaryTreeLevelOrderTraversal {
 	/**
-     * @param root: The root of binary tree.
-     * @return: Level order a list of lists of integer
-     */
-    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
-    	ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
-    	if (root == null) {
-    		return results;
-    	}
-    	Queue<TreeNode> queue = new LinkedList<TreeNode>();
-    	ArrayList<Integer> result = new ArrayList<Integer>();
-    	result.add(root.val);
-    	results.add(result);
-    	queue.add(root);
-    	Queue<Integer> levels = new LinkedList<Integer>();
-    	levels.add(0);
-    	result = new ArrayList<Integer>();
-    	while (!queue.isEmpty()) {
-    		TreeNode node = queue.remove();
-    		int level = levels.remove();
-    		if (node.left != null) {
-    			queue.add(node.left);
-    			levels.add(level + 1);
-    			result.add(node.left.val);
-    		}
-    		if (node.right != null) {
-    			queue.add(node.right);
-    			levels.add(level + 1);
-    			result.add(node.right.val);
-    		}
-    		if (levels.isEmpty() || level != levels.peek()) {
-    			if (!result.isEmpty()) {
-            		results.add(result);
-            	}
-    			result = new ArrayList<Integer>();
-    		}
-    	}
-    	
-    	return results;
-    }
+	 * @param root: The root of binary tree.
+	 * @return: Level order a list of lists of integer
+	 */
+	public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+		ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			// notice here
+			int len = queue.size();
+			ArrayList<Integer> subres = new ArrayList<>();
+			for (int i = 0; i < len; i++) {
+				TreeNode node = queue.remove();
+				if (node == null) {
+					continue;
+				}
+				// check visited if it is a graph rather than a tree
+				subres.add(node.val);
+				queue.add(node.left);
+				queue.add(node.right);
+			}
+			if (subres.size() != 0) {
+				res.add(subres);
+			}
+		}
+
+		return res;
+	}
 }
