917bbd8f1fdd0a2b7d734e5204fc88b1abf0753e
HaoFu <jamesfuhao@gamil.com>
2015-12-08 21:18:42 -0800

    climbing stairs

--- src/DynamicProgrammingI/ClimbingStairs.java
+++ src/DynamicProgrammingI/ClimbingStairs.java
@@ -6,22 +6,20 @@ public class ClimbingStairs {
      * @return: An integer
      */
     public int climbStairs(int n) {
-    	if (n == 0) {
-    		return 0;
-    	}
-    	
-    	int[] f = new int[n + 1];
-    	
-    	// init
-    	f[0] = 1;
-    	f[1] = 1;
-    	
-    	// loop
-    	for (int i = 2; i <= n; i++) {
-    		f[i] = f[i - 1] + f[i - 2];
-    	}
-    	
-    	return f[n];
+		if (n == 0) {
+			return 1;
+		}
+		// init
+		int[] f = new int[3];
+		f[0] = 1;
+		f[1] = 2;
+
+		// loop
+		for (int i = 3; i <= n; i++) {
+			f[(i - 1) % 3] = f[(i - 2) % 3] + f[(i - 3) % 3];
+		}
+
+		return f[(n - 1) % 3];
     }
 
 }
