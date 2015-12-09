8563b380222de0fdf642a87ff005ce147f34290c
HaoFu <jamesfuhao@gamil.com>
2015-12-08 17:54:53 -0800

    max sum path

--- src/BinaryTreeAndDivideConquer/BinaryTreeMaximumPathSumDC.java
+++ src/BinaryTreeAndDivideConquer/BinaryTreeMaximumPathSumDC.java
@@ -1,49 +1,38 @@
 package BinaryTreeAndDivideConquer;
 
 public class BinaryTreeMaximumPathSumDC {
-	private class ResultType {
-		// max to store max value of all possible solutions
-		// single path for node.val + left or right or 0 that used for return to parent
-		int max, singlePath;
-		
-		ResultType(int singlePath, int max) {
-			this.max = max;
-			this.singlePath = singlePath;
-		}
-	}
-	
+	/**
+	 * @param root: The root of binary tree.
+	 * @return: An integer.
+	 */
 	public int maxPathSum(TreeNode root) {
-		ResultType result = maxPathSumHelper(root);
-		return result.max;
+		ResultType res = helper(root);
+		return res.totalMax;
 	}
-		
-	private ResultType maxPathSumHelper(TreeNode root) {
-		if (root == null) {
-			return new ResultType(0, Integer.MIN_VALUE);
+
+	private ResultType helper(TreeNode node) {
+		if (node == null) {
+			return new ResultType(Integer.MIN_VALUE, 0);
 		}
-		
-		ResultType left = maxPathSumHelper(root.left);
-		ResultType right = maxPathSumHelper(root.right);
-		
-		int tmp = root.val;
-    	
-    	if (left.singlePath > 0) {
-    		tmp += left.singlePath;
-    	}
-    	if (right.singlePath > 0) {
-    		tmp += right.singlePath;
-    	}
-    	
-    	int max = Math.max(left.max, right.max);
-    	
-    	if (tmp > max) {
-    		max = tmp;
-    	}
-		
-		int singlePath = root.val;
-		singlePath += Math.max(Math.max(left.singlePath, right.singlePath), 0);
-		
-		return new ResultType(singlePath, max);
+
+		ResultType left = helper(node.left);
+		ResultType right = helper(node.right);
+		int onePath = Math.max(0, Math.max(left.oneMax, right.oneMax));
+		onePath += node.val;
+		int totalMax = Math.max(onePath, Math.max(left.totalMax, right.totalMax));
+		totalMax = Math.max(totalMax, left.oneMax + right.oneMax + node.val);
+
+		return new ResultType(totalMax, onePath);
+	}
+
+	private class ResultType {
+		int totalMax; // store max value of all possible solutions
+		int oneMax; // single path for node.val + left or right or 0 that used for return to parent
+
+		ResultType(int totalMax, int oneMax) {
+			this.totalMax = totalMax;
+			this.oneMax = oneMax;
+		}
 	}
 	
 }
