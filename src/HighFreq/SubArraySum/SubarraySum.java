90b0993cd58f849a161a33d1569a321bf1232795
Hao Fu <jamesfuhao@gmail.com>
2015-12-11 23:14:12 -0800

    subarray sum

--- src/HighFreq/SubArraySum/SubarraySum.java
+++ src/HighFreq/SubArraySum/SubarraySum.java
@@ -2,33 +2,30 @@ package HighFreq.SubArraySum;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class SubarraySum {
 	/**
-     * @param nums: A list of integers
-     * @return: A list of integers includes the index of the first number 
-     *          and the index of the last number
-     */
-    public ArrayList<Integer> subarraySum(int[] nums) {
-    	HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-    	ArrayList<Integer> result = new ArrayList<Integer>();
-    	int sum = 0;
-    	for (int i = 0; i < nums.length; i++) {
-    		sum += nums[i];
-    		if (sum == 0) {
-    			result.add(0);
-    			result.add(i);
-    			return result;
-    		}
-    		if (map.containsKey(sum)) {
-    			result.add(map.get(sum) + 1);
-    			result.add(i);
-    			return result;
-    		}
-    		map.put(sum, i);
-    	}
-    	
-    	return result;
-    }
+	 * @param nums: A list of integers
+	 * @return: A list of integers includes the index of the first number
+	 *          and the index of the last number
+	 */
+	public ArrayList<Integer> subarraySum(int[] nums) {
+		Map<Integer, Integer> preSum = new HashMap<>();
+		ArrayList<Integer> res = new ArrayList<>();
+		preSum.put(0, -1); // to handle cases like [-1, 1]
+		for (int i = 0, sum = 0; i < nums.length; i++) {
+			sum += nums[i];
+			if (preSum.containsKey(sum)) {
+				res.add(preSum.get(sum) + 1);
+				res.add(i);
+				return res;
+			} else {
+				preSum.put(sum, i);
+			}
+		}
+
+		return res;
+	}
 
 }
