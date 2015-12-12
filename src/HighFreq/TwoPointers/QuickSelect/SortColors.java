407ed586d14d65ac60c8f3e98a3f11ecfe15a937
Hao Fu <jamesfuhao@gmail.com>
2015-12-12 14:46:32 -0800

    sort colors

--- src/HighFreq/TwoPointers/QuickSelect/SortColors.java
+++ src/HighFreq/TwoPointers/QuickSelect/SortColors.java
@@ -2,33 +2,30 @@ package HighFreq.TwoPointers.QuickSelect;
 
 public class SortColors {
 	/**
-     * @param nums: A list of integer which is 0, 1 or 2 
-     * @return: nothing
-     */
-    public void sortColors(int[] nums) {
-    	int pos1Start = partition(0, nums.length - 1, 1, nums);
-    	partition(pos1Start, nums.length - 1, 2, nums);
-    }
-    
-    private int partition(int start, int end, int target, int[] nums) {
-    	int i = start, j = end;
-    	while (i <= j) {
-    		while (i <= j && nums[i] < target) {
-    			i++;
-    		}
-    		while (i <= j && nums[j] >= target) {
-    			j--;
-    		}
-    		if (i <= j) {
-    			int tmp = nums[i];
-    			nums[i] = nums[j];
-    			nums[j] = tmp;
-    			i++;
-    			j--;
-    		}
-    	}
-    	
-    	return i;
-    }
+	 * @param nums: A list of integer which is 0, 1 or 2
+	 * @return: nothing
+	 */
+	public void sortColors(int[] nums) {
+		int i = 0, k = 0, j = nums.length - 1;
+		int target = 1;
+		while (i <= j) {
+			if (nums[i] < target) {
+				swap(nums, i, k);
+				i++;
+				k++;
+			} else if(nums[i] == target) {
+				i++;
+			} else {
+				swap(nums, i, j);
+				j--;
+			}
+		}
+	}
+
+	private void swap(int[] nums, int i, int j) {
+		int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
+	}
 
 }
