4da959adb226f6d6d2e011a544d4d935bbf36f98
Hao Fu <jamesfuhao@gmail.com>
2015-12-12 23:54:42 -0800

    subarray sum cloest

--- src/HighFreq/SubArraySum/SubarraySumClosest.java
+++ src/HighFreq/SubArraySum/SubarraySumClosest.java
@@ -6,52 +6,51 @@ import java.util.Comparator;
 
 public class SubarraySumClosest {
 	/**
-     * @param nums: A list of integers
-     * @return: A list of integers includes the index of the first number 
-     *          and the index of the last number
-     */
-    public ArrayList<Integer> subarraySumClosest(int[] nums) {
-    	ArrayList<Integer> res = new ArrayList<Integer>();
-    	if (nums.length == 1) {
-    		res.add(0);
-    		res.add(0);
-    		return res;
-    	}
-    	class Pair {
-    		int sum;
-    		int index;
-    		Pair (int sum, int index) {
-    			this.sum = sum;
-    			this.index = index;
-    		}
-    	}
-    	Pair[] sum = new Pair[nums.length + 1];
-    	sum[0] = new Pair(0, 0);
-    	for (int i = 1; i <= nums.length; i++) {
-    		sum[i] = new Pair(sum[i - 1].sum + nums[i - 1], i);
-    	}
-    	Arrays.sort(sum, new Comparator<Pair> () {
-    		public int compare (Pair a, Pair b) {
-    			return a.sum - b.sum;
-    		}
-    	});
-    	
-    	int cloest = Integer.MAX_VALUE;
-    	
-    	for (int i = 1; i <= nums.length; i++) {
-    		if (cloest > sum[i].sum - sum[i - 1].sum) {
-    			cloest = sum[i].sum - sum[i - 1].sum;
-    			res.clear();
-    			int[] tmp = new int[2];
-    			tmp[0] = sum[i - 1].index - 1;
-    			tmp[1] = sum[i].index - 1;
-    			Arrays.sort(tmp);
-    			res.add(tmp[0] + 1);
-    			res.add(tmp[1]);
-    		}
-    	}
-    	
-    	return res;
-    }
+	 * @param nums: A list of integers
+	 * @return: A list of integers includes the index of the first number
+	 *          and the index of the last number
+	 */
+	public int[] subarraySumClosest(int[] nums) {
+		int n = nums.length;
+		SumElement[] sums = new SumElement[n];
+
+		for (int i = 0, sum = 0; i < n; i++) {
+			sum += nums[i];
+			sums[i] = new SumElement(sum, i);
+		}
+
+		Arrays.sort(sums, new SumElementComparator());
+		int min = Integer.MAX_VALUE;
+		int[] res = new int[2];
+		for (int i = 1; i < n; i++) {
+			if (sums[i].sum - sums[i - 1].sum < min) {
+				if (sums[i - 1].index < sums[i].index) {
+					res[0] = sums[i - 1].index + 1;
+					res[1] = sums[i].index;
+				} else {
+					res[0] = sums[i].index + 1;
+					res[1] = sums[i - 1].index;
+				}
+				min = sums[i].sum - sums[i - 1].sum;
+			}
+		}
+
+		return res;
+	}
+
+	class SumElement {
+		int sum;
+		int index;
+		SumElement(int sum, int index) {
+			this.sum = sum;
+			this.index = index;
+		}
+	}
+
+	class SumElementComparator implements Comparator<SumElement> {
+		public int compare(SumElement a, SumElement b) {
+			return a.sum - b.sum;
+		}
+	}
 
 }
