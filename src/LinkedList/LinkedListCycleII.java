e4bae4aa035f00ac25d38fa2a951ce738e019c56
HaoFu <jamesfuhao@gamil.com>
2015-12-14 14:49:47 -0800

    linked list cycle ii

--- src/LinkedList/LinkedListCycleII.java
+++ src/LinkedList/LinkedListCycleII.java
@@ -4,28 +4,30 @@ import java.util.HashMap;
 
 public class LinkedListCycleII {
 	/**
-     * @param head: The first node of linked list.
-     * @return: The node where the cycle begins. 
-     *           if there is no cycle, return null
-     */
-    public ListNode detectCycle(ListNode head) {  
-    	if (head == null) {
-    		return null;
-    	}
-    	
-    	HashMap<ListNode, Integer> hash = new HashMap<ListNode, Integer>();
-    	
-    	while (head != null) {
-    		if (hash.containsKey(head)) {
-    			return head;
-    			
-    		} else {
-    			hash.put(head, 0);
-    			head = head.next;
-    		}
-    	}
-    	
-    	return null;
-    }
+	 * @param head: The first node of linked list.
+	 * @return: The node where the cycle begins.
+	 *           if there is no cycle, return null
+	 */
+	public ListNode detectCycle(ListNode head) {
+		if (head == null || head.next==null) {
+			return null;
+		}
+
+		ListNode slow = head, fast = head.next;
+		while (slow != fast) {
+			slow = slow.next;
+			if (fast == null || fast.next == null) {
+				return null;
+			}
+			fast = fast.next.next;
+		}
+
+		while (head != slow.next) {
+			slow = slow.next;
+			head = head.next;
+		}
+
+		return head;
+	}
 
 }
