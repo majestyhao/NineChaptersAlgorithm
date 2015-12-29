98d677bfeebe04c0e3fc338cb7fc05d4a27812d1
Hao Fu <jamesfuhao@gmail.com>
2015-12-29 00:01:27 -0800

    lcs

--- src/DataStructure/Hash/LongestConsecutiveSequence.java
+++ src/DataStructure/Hash/LongestConsecutiveSequence.java
@@ -1,47 +1,39 @@
 package DataStructure.Hash;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class LongestConsecutiveSequence {
 	/**
-     * @param nums: A list of integers
-     * @return an integer
-     */
-    public int longestConsecutive(int[] num) {
-    	HashSet<Integer> set = new HashSet<Integer>();
-    	for (int i = 0; i < num.length; i++) {
-    		if (set.contains(num[i])) {
-    			continue;
-    		}
-    		set.add(num[i]);
-    	}
-    	
-    	int max = 1;
-    	HashSet<Integer> met = new HashSet<Integer>();
-    	for (int i = 0; i < num.length; i++) {
-    		if (met.contains(num[i])) {
-    			continue;
-    		}
-    		met.add(num[i]);
-    		int tmp = num[i];
-    		int count = 1;
-    		while (set.contains(tmp - 1)) {
-    			met.add(tmp - 1);
-    			tmp = tmp - 1;
-    			count++;
-    		}
-    		tmp = num[i];
-    		while (set.contains(tmp + 1)) {
-    			met.add(tmp + 1);
-    			tmp = tmp + 1;
-    			count++;
-    		}
-    		if (count > max) {
-    			max = count;
-    		}
-    	}
-    	
-    	return max;
-    }	
+	 * @param nums: A list of integers
+	 * @return an integer
+	 */
+	public int longestConsecutive(int[] nums) {
+		Set<Integer> visited = new HashSet<>();
+		int n = nums.length;
+
+		for (int i = 0; i < n; i++) {
+			visited.add(nums[i]);
+		}
+
+		int max = 0;
+		for (int i = 0; i < n; i++) {
+			int num = nums[i], count = 1;
+			int previous = num - 1, next = num + 1;
+			while (!visited.isEmpty() && visited.contains(previous)) {
+				visited.remove(previous);
+				previous--;
+				count++;
+			}
+			while (!visited.isEmpty() && visited.contains(next)) {
+				visited.remove(next);
+				next++;
+				count++;
+			}
+			max = Math.max(max, count);
+		}
+
+		return max;
+	}
 
 }
